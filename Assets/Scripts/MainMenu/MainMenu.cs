2043cb5bc3d7f701667176a7acf86a7b78f31299
Francisco de Paula Mediavilla Garcia <92514622+CMediavilla16@users.noreply.github.com>
2026-03-13 02:01:41 +0100

    Menu inicio

---
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,18 @@
+using Unity.VectorGraphics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
