ee25f1b601f2052c8268113605b4daec1fb49cfb
Francisco de Paula Mediavilla Garcia <92514622+CMediavilla16@users.noreply.github.com>
2026-03-13 23:46:24 +0100

    Justo antes de tocar las SKINS

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -1,9 +1,36 @@
+using TMPro;
 using Unity.VectorGraphics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+
+    public GameObject optionsPanel;
+    public GameObject buttonsPanel;
+
+    public TMP_Dropdown qualityDropDown;
+
+
+    private void Start()
+    {
+        int savedQuality = PlayerPrefs.GetInt("QualityLevel", -1);
+        
+        if (savedQuality == -1)
+        {
+            savedQuality = QualitySettings.GetQualityLevel();
+
+        }
+
+        qualityDropDown.ClearOptions();
+        qualityDropDown.AddOptions(new System.Collections.Generic.List<string>(QualitySettings.names));
+
+        qualityDropDown.value = savedQuality;
+
+        qualityDropDown.onValueChanged.AddListener(OnQualityChange);
+
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene("Level1");
@@ -12,6 +39,26 @@ public class MainMenu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+
+    public void OpenOptions()
+    {
+        buttonsPanel.SetActive(false);
+        optionsPanel.SetActive(true);
+    }
+
+    public void CloseOptions()
+    {
+        buttonsPanel.SetActive(true );  
+        optionsPanel.SetActive(false);
+    }
+
+
+    public void OnQualityChange(int index)
+    {
+        QualitySettings.SetQualityLevel(index, true);
+        PlayerPrefs.SetInt("QualityLevel", index);
     }
 
 
